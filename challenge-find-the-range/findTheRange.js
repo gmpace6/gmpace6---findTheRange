9d3a1e691c8224aab13855e8adb2ad2112aef3ee
Garrett Pace <elderpace6@gmail.com>
2023-04-18 08:42:55 -0600

    done

---
+++ challenge-find-the-range/findTheRange.js
@@ -0,0 +1,19 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+
+// Write your solution below:
+
+
+let smallest = arr[0]
+let largest = arr[0]
+
+
+for(let i = 0; i < arr.length; i++){
+  if(arr[i] < smallest){
+    smallest = arr[i]
+  } else if(largest < arr[i]){
+    largest = arr[i]
+  }
+}
+
+console.log(smallest, largest)
